5d65e27a1a83cc438735a750e8838d377e9eb53c
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2017-10-16 11:47:59 -0400

    fix storybook freactal

--- src/components/Screens.stories.tsx
+++ src/components/Screens.stories.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Users from './Users';
 import StoryRouter from 'storybook-router';
+import ProviderUsers from 'stateProviders/provideUser';
+
+const StateProviders = ProviderUsers(({ children }) => children);
 
 storiesOf('Screens', module)
   .addDecorator(StoryRouter())
+  .addDecorator(storyFn => <StateProviders>{storyFn()}</StateProviders>)
   .add('Users', () => <Users />);
