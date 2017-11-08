b0a0613cd0bcc9ba33a80dc98557b8fce33b1c16
Chang Wang <cheapsteak@gmail.com>
2017-11-08 14:04:48 -0500

    simplify routes (#96)

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,12 @@ import ResourceRoute from 'components/ResourceRoute';
 import NoAccess from 'components/NoAccess';
 import Nav from 'components/Nav';
 import RESOURCE_MAP from 'common/RESOURCE_MAP';
+import Aux from 'components/Aux';
 
 const enhance = compose(provideLoggedInUser);
 
-const ProtectedRoute = injectState(({ renderLogin, component, state, ...rest }) => (
-  <Route {...rest} component={state.loggedInUserToken ? component : renderLogin && Login} />
+const ProtectedRoute = injectState(({ component, state, ...rest }) => (
+  <Route {...rest} component={state.loggedInUserToken ? component : Login} />
 ));
 
 class App extends React.Component<any, any> {
@@ -23,24 +24,26 @@ class App extends React.Component<any, any> {
     return (
       <Router>
         <div style={{ height: '100%', display: 'flex' }}>
-          <ProtectedRoute
-            path={`/(${Object.keys(RESOURCE_MAP).join(
-              '|',
-            )})/:id?/:subResourceType?/:subResourceId?`}
-            exact
-            component={Nav}
-          />
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/no-access" exact component={NoAccess} />
-            {Object.keys(RESOURCE_MAP).map(key => (
-              <ProtectedRoute
-                key={key}
-                path={`/${key}/:id?/:subResourceType?/:subResourceId?`}
-                component={props => <ResourceRoute {...props} type={key} />}
-                renderLogin
-              />
-            ))}
+            <ProtectedRoute
+              component={() => (
+                <Aux>
+                  <Nav />
+                  <Switch>
+                    {Object.keys(RESOURCE_MAP).map(key => (
+                      <ProtectedRoute
+                        key={key}
+                        path={`/${key}/:id?/:subResourceType?/:subResourceId?`}
+                        component={props => <ResourceRoute {...props} type={key} />}
+                        renderLogin
+                      />
+                    ))}
+                  </Switch>
+                </Aux>
+              )}
+            />
           </Switch>
         </div>
       </Router>
