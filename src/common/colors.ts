67880c91cdaf53bbe253ce397c46f4baeb201db9
Ann Catton <ann.catton@oicr.on.ca>
2020-01-22 16:44:09 -0500

    remove unused blue

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -4,7 +4,6 @@ const WHITE = '#FFF';
 const LIGHT_GREY = '#F3F3F3'; // background
 const GREY = '#DFDEDF';
 const DARK_GREY = '#666';
-const BLUE = '#00A1D8'; // lighter blue. not currently used
 const DARK_BLUE = '#003055';
 const MEDIUM_BLUE = '#1f7abf';
 const LIGHT_TEAL = '#e5f7f4';
