ffe9d2dc0c353146b4d6086b87c82d0c8312229f
Ann Catton <ann.catton@oicr.on.ca>
2021-04-13 18:13:08 -0400

    update orcid icon, copyjwt

--- src/theme/dimensions.ts
+++ src/theme/dimensions.ts
@@ -2,6 +2,12 @@ const dimensions = {
   contentPanel: {
     width: 510,
   },
+  fieldNameWidths: {
+    application: 5,
+    group: 3,
+    policy: 3,
+    user: 4,
+  },
 };
 
 export default dimensions;
