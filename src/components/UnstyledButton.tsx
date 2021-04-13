55904ae3529b76af34db185dee154f8227e1e02d
Ann Catton <ann.catton@oicr.on.ca>
2021-04-13 15:20:33 -0400

    ApiKeysTable emotion, colors theme update

--- src/components/UnstyledButton.tsx
+++ src/components/UnstyledButton.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import { css } from 'glamor';
+/** @jsxImportSource @emotion/react */
+import React, { ReactNode } from 'react';
+import styled from '@emotion/styled';
+import { CSSObject } from '@emotion/react';
 
-const styles = {
-  button: {
-    backgroundColor: 'inherit',
-    border: 'none',
-    cursor: 'pointer',
-    outline: 'none',
-    padding: 0,
-  },
+const Button = styled('button')`
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+`;
+
+const UnstyledButton = ({
+  children,
+  style,
+  onClick = () => null,
+  className = '',
+}: {
+  children: ReactNode;
+  style?: CSSObject;
+  onClick?: () => void;
+  className?: string;
+}) => {
+  return (
+    <Button css={style} onClick={onClick} className={className}>
+      {children}
+    </Button>
+  );
 };
 
-export default ({ children, style, className = '', ...props }: any) => (
-  <button {...props} className={`${className} ${css(styles.button, style)}`}>
-    {children}
-  </button>
-);
+export default UnstyledButton;
