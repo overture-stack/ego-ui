ca48183cd41eadec9d2eacc9ce32f29770681b13
Ann Catton <anncatton@gmail.com>
2022-06-10 09:37:42 -0400

    rename panelcomponents

--- src/components/Content/PanelComponents.tsx
+++ src/components/Content/PanelComponents.tsx
@@ -1,10 +1,12 @@
-// export { default } from './Content';
-
 import { ResourceType } from 'common/enums';
 import UserContentPanel from './UserContentPanel';
 
+// convert to ParentResource?
 const PanelComponents = {
   [ResourceType.USERS]: UserContentPanel,
+  [ResourceType.GROUPS]: () => <div />,
+  [ResourceType.APPLICATIONS]: () => <div />,
+  [ResourceType.POLICIES]: () => <div />,
 };
 
 export default PanelComponents;
