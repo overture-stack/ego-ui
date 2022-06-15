424d98422028eb8e5ad5ded22e3ec1683d9b09e8
Ann Catton <anncatton@gmail.com>
2022-06-15 12:21:05 -0400

    rename entity type files

--- src/common/typedefs/index.ts
+++ src/common/typedefs/index.ts
@@ -1,13 +1,12 @@
 import { Application } from 'common/typedefs/Application';
 import { Group } from 'common/typedefs/Group';
 import { User } from 'common/typedefs/User';
-import { ApiKey } from './ApiKey';
 import { Permission } from './Permission';
 import { Policy } from './Policy';
 
 export type Entity = User | Group | Application | Policy | Permission;
 
-export * from './User.d';
-export * from './Group.d';
-export * from './Application.d';
-export * from './Policy.d';
+export * from './User';
+export * from './Group';
+export * from './Application';
+export * from './Policy';
